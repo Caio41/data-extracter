7b9890787e09cf1185340db61f7cfece196afe74
Caio <caiaoviniciuscc@gmail.com>
2025-02-25 18:29:52 -0300

    feat: add rota de img para doc

--- backend/routes/data.py
+++ backend/routes/data.py
@@ -2,7 +2,7 @@ from io import BytesIO, StringIO
 import os
 from tempfile import NamedTemporaryFile
 from fastapi import APIRouter, File, UploadFile
-from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.responses import StreamingResponse
 import numpy as np
 import easyocr
 import pytesseract
@@ -11,29 +11,49 @@ from PIL import Image
 import cv2
 import ocrmypdf
 import csv
-from collections import Counter
+from docx import Document
+
 
 router = APIRouter()
 
-reader = easyocr.Reader(['pt', 'en'])
+reader = easyocr.Reader(["pt", "en"])
+
 
 # easyocr puro, so funciona pra imagens (png, jpg, etc)
-@router.post('/easyocr-png')
+@router.post("/easyocr-png")
 def teste(arquivo: UploadFile = File(...)):
     with NamedTemporaryFile(delete=False) as temp_file:
-        
         temp_file.write(arquivo.file.read())
-        temp_file_path = temp_file.name  
-    
-    
+        temp_file_path = temp_file.name
+
     result = reader.readtext(temp_file_path, detail=0)
-    
-    
+
     os.remove(temp_file_path)
     return result
 
+
+@router.post("/img-to-doc")
+async def img_to_doc(arquivo: UploadFile = File(...)):
+    file_content = await arquivo.read()
+    result = reader.readtext(file_content, detail=0)
+
+    doc = Document()
+    for text in result:
+        doc.add_paragraph(text)
+
+    output = BytesIO()
+    doc.save(output)
+    output.seek(0)
+
+    return StreamingResponse(
+        output,
+        media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        headers={"Content-Disposition": "attachment; filename=teste.docx"},
+    )
+
+
 # usa ocrmypdf e fitz
-@router.post('/easyocr-pdf')
+@router.post("/easyocr-pdf")
 async def easyocr_pdf(arquivo: UploadFile = File(...)):
     file_content = await arquivo.read()
 
@@ -43,52 +63,52 @@ async def easyocr_pdf(arquivo: UploadFile = File(...)):
         tmp_file.write(file_content)
         tmp_file_path = tmp_file.name
 
-    ocrmypdf.ocr(tmp_file_path, output, language='por')
+    ocrmypdf.ocr(tmp_file_path, output, language="por")
 
     output.seek(0)
     output_content = output.read()
     doc = fitz.open(stream=output_content)
-    texto_extraido = ''
+    texto_extraido = ""
     for page_num in range(len(doc)):
         page = doc.load_page(page_num)
-        texto_extraido += page.get_text('text')
+        texto_extraido += page.get_text("text")
 
     print(texto_extraido)
     return texto_extraido
-
 
 
 # tesseract com fitz pra tentar ler pdf
-@router.post('/tesseract')
+@router.post("/tesseract")
 async def tesseract_teste(arquivo: UploadFile = File(...)):
     file_content = await arquivo.read()
 
-    doc = fitz.open(stream=file_content, filetype='pdf')
+    doc = fitz.open(stream=file_content, filetype="pdf")
 
-    texto_extraido = ''
+    texto_extraido = ""
     for page in doc:
         pix = page.get_pixmap()
         img = Image.open(BytesIO(pix.tobytes("png")))
-       # img = preprocessar_imagem(img)
-        text = pytesseract.image_to_string(img, lang='por')
-        texto_extraido += text + '\n'
+        # img = preprocessar_imagem(img)
+        text = pytesseract.image_to_string(img, lang="por")
+        texto_extraido += text + "\n"
 
     print(texto_extraido)
     return texto_extraido
 
-@router.post('/tesseract/tabela')
+
+@router.post("/tesseract/tabela")
 async def tesseract_extrair_tabela(arquivo: UploadFile = File(...)):
     file_content = await arquivo.read()
 
-    doc = fitz.open(stream=file_content, filetype='pdf')
+    doc = fitz.open(stream=file_content, filetype="pdf")
 
-    texto_extraido = ''
+    texto_extraido = ""
     for page in doc:
         pix = page.get_pixmap()
         img = Image.open(BytesIO(pix.tobytes("png")))
-       # img = preprocessar_imagem(img)
-        text = pytesseract.image_to_string(img, lang='por')
-        texto_extraido += text + '\n'
+        # img = preprocessar_imagem(img)
+        text = pytesseract.image_to_string(img, lang="por")
+        texto_extraido += text + "\n"
 
     rows = estimar_linhas(texto_extraido)
 
@@ -111,29 +131,33 @@ async def tesseract_extrair_tabela(arquivo: UploadFile = File(...)):
         wr.writerow(row)
 
     output.seek(0)
-    return StreamingResponse(output, media_type="text/csv", headers={"Content-Disposition": "attachment; filename=table.csv"})
-
- 
+    return StreamingResponse(
+        output,
+        media_type="text/csv",
+        headers={"Content-Disposition": "attachment; filename=table.csv"},
+    )
 
 
 def split_list(l, n):
     splitted = []
 
     for i in range(0, len(l), n):
-        splitted.append(l[i:i + n])
+        splitted.append(l[i : i + n])
 
     return splitted
+
 
 def estimar_linhas(input_text: str):
     lines = len(input_text.strip().split("\n"))
     return lines
 
+
 def preprocessar_imagem(img):
     img_cv = np.array(img)
     img_cv = cv2.cvtColor(img_cv, cv2.COLOR_RGB2GRAY)
-    
+
     img_cv = cv2.adaptiveThreshold(
         img_cv, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
     )
-    
+
     return Image.fromarray(img_cv)
