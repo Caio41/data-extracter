74ff383acf4816e1313bb4a383d6149ca3e4bf3e
Álvaro Peixoto <alvaro.peixoto@dcomp.ufs.br>
2025-03-09 19:43:43 -0300

    chore: ajustes de middleware para uso do front

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes import tabelas, documents, experimentos
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
+)
 
 app.include_router(tabelas.router, prefix="/tabelas", tags=["tabelas"])
 app.include_router(experimentos.router, prefix="/experimentos", tags=["experimentos"])
